ad43837450e3d75cd38c03d58be8753dac029c36
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 01:54:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
-def my_each(par) 
-    i=0
-    while i<par.length
-      yield par[i]
-      i+=1
-   end
-   par
+def my_each(array)
+    i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
 end
 # collection = [1, 2, 3, 4]
 # my_each(collection) do |i|
