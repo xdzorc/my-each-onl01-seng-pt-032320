8f4f885c2bb4d62b4634d4cb2455f25b97722091
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 01:52:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,7 +6,7 @@ def my_each(par)
    end
    par
 end
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
-end
+# collection = [1, 2, 3, 4]
+# my_each(collection) do |i|
+#   puts i
+# end
